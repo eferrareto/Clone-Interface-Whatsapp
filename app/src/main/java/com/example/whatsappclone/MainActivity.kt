bd0323af427c5e9ce551ab4aa44813ddc18dc3a2
Erick Ferrareto <e.ferraretowork@gmail.com>
2022-11-15 13:41:45 -0300

    working with frgaments

--- app/src/main/java/com/example/whatsappclone/MainActivity.kt
+++ app/src/main/java/com/example/whatsappclone/MainActivity.kt
@@ -5,8 +5,12 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.whatsappclone.model.DataClassRecycler
@@ -14,10 +18,25 @@ import com.example.whatsappclone.model.DataClassRecycler
 class MainActivity : AppCompatActivity() {
 
     private lateinit var recycler : RecyclerView
+    private lateinit var buttonFragment1: Button
+    private lateinit var buttonFragment2 : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null){
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<fragmentFirst>(R.id.fragment)
+            }
+        }
+
+        buttonFragment1 = findViewById(R.id.fragment1)
+        buttonFragment1.setOnClickListener {
+
+        }
+
+
 
         val listNames = mutableListOf<DataClassRecycler>()
 
---
+++ app/src/main/java/com/example/whatsappclone/fragmentFirst.kt
@@ -0,0 +1,15 @@
+package com.example.whatsappclone
+
+import android.os.Bundle
+import androidx.core.graphics.Insets.add
+import androidx.fragment.app.Fragment
+
+class fragmentFirst : Fragment(R.layout.fragment1) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (savedInstanceState == null){
+            
+        }
+    }
+}
